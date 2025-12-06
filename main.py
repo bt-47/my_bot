d6cf892ec5b29a62dffcf4f5b9e3ddee2197b356
Lobov Egor <egor.lobov.04.02@gmail.com>
2025-12-06 21:40:32 +0200

    Deleted files

--- main.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def create_passwords(n_len: int, n_count: int) -> None:
-    with open('passwords', 'w', encoding='utf-8') as file:
-        a = string.ascii_letters + string.digits
-        file.writelines(''.join([random.choice(a) for _ in range(n_len)]) + '\n' for _ in range(n_count))
-
-create_passwords(10, 10)
